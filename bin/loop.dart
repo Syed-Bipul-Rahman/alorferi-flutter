75b048ade5c4a6706885e04aa2dd9ee3990364f8
Syed Bipul Rahman <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2024-03-20 12:26:24 +0600

    odd even

--- bin/loop.dart
+++ bin/loop.dart
@@ -1,8 +1,24 @@
 main() {
-  var i = 0;
-  for (; i <= 10; ++i) {
-    print(i);
-  }
+  // var i = 0;
+  // for (; i <= 10; ++i) {
+  //   print(i);
+  // }
+  // print(i);
 
-  print(i);
+//printing ODD NUMBER
+
+  // for (var i = 0; i <= 100; i++) {
+  //   if (i % 2 == 0)
+  //     continue;
+  //   else
+  //     print("$i Is ODD NUMBER");
+  // }
+
+  //odd and even at same time
+  for (var i = 0; i <= 100; i++) {
+    if (i % 2 == 0)
+      print("$i Is EVEN number");
+    else
+      print("$i Is ODD number");
+  }
 }
