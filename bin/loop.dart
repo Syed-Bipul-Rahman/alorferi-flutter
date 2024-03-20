1a8831e4df08e6842a39c6401876354bd18984be
Syed Bipul Rahman <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2024-03-20 12:50:36 +0600

    typing effect with loog and duration

--- bin/loop.dart
+++ bin/loop.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 main() {
   // var i = 0;
   // for (; i <= 10; ++i) {
@@ -15,10 +17,22 @@ main() {
   // }
 
   //odd and even at same time
-  for (var i = 0; i <= 100; i++) {
-    if (i % 2 == 0)
-      print("$i Is EVEN number");
-    else
-      print("$i Is ODD number");
+  // for (var i = 0; i <= 100; i++) {
+  //   if (i % 2 == 0)
+  //     print("$i Is EVEN number");
+  //   else
+  //     print("$i Is ODD number");
+  // }
+
+  //typing effect with loop and duration
+  var name="Syed Bipul Rahman";
+
+  for(var i=0; i<name.length; i++){
+    // print(name[i]);
+    sleep(Duration(milliseconds: 250));
+    stdout.write(name[i]);
   }
+
+
+
 }
